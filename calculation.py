9de5d9b47e5b2e317beded7456e195b5d3dd724e
Dennis Kimutai <kimutaidennisbrian@gmail.com>
2024-11-12 10:49:43 +0300

    calculation

---
+++ calculation.py
@@ -0,0 +1,13 @@
+firstNumber = int(input("Please provide first number:"))
+
+secondNumber = int(input("Please provide second number:"))
+
+multiplication = firstNumber * secondNumber
+
+addition = firstNumber + secondNumber
+
+subtraction = firstNumber- secondNumber
+
+division = firstNumber / secondNumber
+
+print(f"The values of the calculations are: multiplication: {multiplication} addition:{addition} subtraction:{subtraction} division:{division} ")
